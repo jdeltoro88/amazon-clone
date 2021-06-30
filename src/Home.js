b4a253b38d340f82f9b6ed3712202de6016a3273
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-06-29 20:41:39 -0600

    added home component and banner

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,11 @@ import './Home.css';
 function Home() {
     return (
         <div classname='home'>
+            <div className="home__container">
+                <img
+                    className='home__image'
+                    src='https://i1.wp.com/fintechranking.com/wp-content/uploads/2017/03/AMAZON-1200x537.png?fit=1200%2C537&ssl=1' alt='amazon banner'/>
+            </div>
             
         </div>
     )
