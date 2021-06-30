b0fbda585d10dd61fb85efa6257028190b34dae7
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-06-29 20:08:19 -0600

    header options styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Header from "./Header.js"
+import Home from './Home.js'
+
 
 function App() {
   return (
     <div className="App">
       
       <Header />
+      <Home/>
      
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 
 function Header() {
@@ -37,6 +38,14 @@ function Header() {
                             <span className= 'header__optionLineOne'> your</span>
                             <span className='header__optionLineTwo'> Prime </span>
                             
+                        </div>
+
+                        <div className='header__optionBasket'>
+                            <ShoppingBasketIcon/>
+                            <span className="header__optionLineTwo header__basketCount">
+                                0
+
+                            </span>
                         </div>
 
                     </div>
