f97e1f3f8d9dc9a4fdb95e9e0c0ca52ab4097ef9
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-08-25 23:31:48 -0600

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
+import React from " react ";
 import './App.css';
-import Header from "./Header.js"
-import Home from './Home.js'
+import Header from "./Header.js";
+import Home from './Home.js';
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
+
 
 
 function App() {
