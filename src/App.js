bab63e11e70bb1a146b474a66fe06d8481d06d6c
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-09-23 19:16:30 -0600

    checkout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route }
-from "react-router-dom";
 import Checkout from "./Checkout";
+
+
 
 
 
