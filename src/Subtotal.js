3b6a73edc6166d452efbcd801aa4fcbc8f27b7d8
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-10-11 23:01:02 -0600

    sbttl bug

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -12,7 +12,8 @@ function Subtotal() {
                     Subtotal (0 items): <strong>0</strong>
                 </p>
                 <small className='subtotal__gift'>
-                    <input type= "checkbox" /> this order contains a gift
+                    <input type= "checkbox"/> 
+                    this order contains a gift
                 </small>
                 </>
 
