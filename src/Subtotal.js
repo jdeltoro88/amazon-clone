510422e2125d23f29f424d97871c109c800fd506
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-10-12 00:04:13 -0600

    bgg

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -13,7 +13,7 @@ function Subtotal() {
                 </p>
                 <small className='subtotal__gift'>
                     <input type= "checkbox"/> 
-                    gift
+                    gift subtotal
                 </small>
                 </>
 
