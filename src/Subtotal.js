103580fe4c0081cbd92f1d727838c3a6fb981d5a
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-10-27 22:02:53 -0600

    subtotal bug fixed

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,12 +4,12 @@ import CurrencyFormat from 'react-currency-format'
 //subtotal not popullating 
 
 function Subtotal() {
-    return <div className='subtotal'>
+    return ( <div className='subtotal'>
             <CurrencyFormat
             renderText={(value) => (
                 <>
                 <p>
-                    Subtotal ({basket.length} items) : <strong>{` {$value}`}</strong>
+                    Subtotal (0 items) : <strong>0</strong>
                 </p>
                 <small className='subtotal__gift'>
                     <input type= "checkbox"/> 
@@ -19,15 +19,17 @@ function Subtotal() {
 
             )}
             decimalScale={2}
-            value={getBasketTotal(basket)}
+            value={0}
             displayType={'text'}
             thousandSeparator={true}
             prefix={'$'}
             />
+
+            <button>Proceed to checkout</button>
             
             
         </div>
-    
+    )
 }
 
 export default Subtotal
