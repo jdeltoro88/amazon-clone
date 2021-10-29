a66e71a44295e8127cefae1c0c056b066cac0247
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-10-28 21:45:22 -0600

    update

--- StateProvider.js
+++ StateProvider.js
@@ -1,11 +1,11 @@
 import React, { createContext, useContext, useReducer } from 'react';
-
+//data prep
 export const StateContext = createContext();
-
+//wraps app and provides data
 export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider 
     value={useReducer(reducer,initialState)}>
     </StateContext.Provider>
 );
-
+//pull info from data
 export const useStateValue = () => useContext(StateContext);
