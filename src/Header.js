9fe54eaa5462c269c72af5dcde406d8f83d0a9ff
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-11-06 15:55:08 -0600

    context api

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 import { Link } from  "react-router-dom";
+import { useStateValue } from './StateProvider';
+
 
 
 function Header() {
+    const [{basket}] = useStateValue();
+
     return (
         <div className='header'>
             <Link to="/">
@@ -47,7 +52,7 @@ function Header() {
                         <Link to = '/checkout'>
                         <div className='header__optionBasket'>
                             <ShoppingBasketIcon/>
-                            <span className="header__optionLineTwo header__basketCount">0</span>
+                            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
                         </div>
                         </Link>
                         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import './Product.css';
+import { useStateValue } from './StateProvider';
+
 
 function Product({id, title, image, price, rating}) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        //dispatch item into data storage
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+
+            }
+        })
+    }
     return (
         <div className = 'product'>
             <div className='product__info'>
@@ -27,7 +45,7 @@ function Product({id, title, image, price, rating}) {
 
             <img src = {image} alt = "" />
 
-                <button>Add to basket</button>
+                <button onClick={addToBasket}>Add to basket</button>
             
         </div>
     )
